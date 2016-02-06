49f40a5d21baa4178c432fd994e51259cf99d3de
Vinay <vinayknp.iiita@gmail.com>
2016-02-06 17:22:28 +0530

    done with layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Tutorial"
+    assert_select "title", "Tutorial"
   end
 
   test "should get help" do
@@ -17,6 +17,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | Tutorial"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Tutorial"
   end
 
 end

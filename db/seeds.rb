47857c42676963a70cf8a1d9dd7752a3e62a9250
Vinay <vinayknp.iiita@gmail.com>
2016-02-28 19:15:59 +0530

    added my seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Vinay Saini",
+             email: "vinay@indeed.com",
+             password:              "qweqwe",
+             password_confirmation: "qweqwe",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "user-#{n+1}@exmaple.com"
   password = "password"
   User.create!(name:  name,
                email: email,
